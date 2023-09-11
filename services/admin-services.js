acec631e87f6193eb996a3f0f665e8fede7e6dca
taiwania <taiwaniajusto@gmail.com>
2023-09-12 01:25:23 +0800

    Add api error handler and delete restaurant api

--- services/admin-services.js
+++ services/admin-services.js
@@ -6,6 +6,20 @@ const adminServices = {
       callback(null, { restaurants })
     })
       .catch(err => callback(err))
+  },
+
+  deleteRestaurant: (req, callback) => {
+    Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) {
+          const err = new Error('Restaurant not found!')
+          err.status = 404
+          throw err
+        }
+        return restaurant.destroy()
+      })
+      .then(deletedRestaurant => callback(null, { restaurant: deletedRestaurant }))
+      .catch(err => callback(err))
   }
 }
 
